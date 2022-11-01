f07fc19a89b02da3296e9f3e6512740ec352e877
Edson Orivaldo Lessa Junior <84724309+edson-lessa-jr@users.noreply.github.com>
2022-11-01 20:52:03 -0300

    atividade 2

--- src/atividade2/VetorFuncionario.java
+++ src/atividade2/VetorFuncionario.java
@@ -1,6 +1,24 @@
 package atividade2;
 
+import javax.swing.*;
+
 public class VetorFuncionario {
     //Registre o nome, salário do funcionário e o nome dos dependentes.
     // Exiba no final as informações e um acréscimo de R$20,00 por dependente ao salário
+    public static void main(String[] args) {
+        String nome = JOptionPane.showInputDialog("Informe o nome do Funcionário");
+        double salario = Double.parseDouble(JOptionPane.showInputDialog("Informe o salário do funcionário"));
+        int qtdDependentes = Integer.parseInt(JOptionPane.showInputDialog("Informe a quantidade de dependentes"));
+        String[] nomeDependentes = new String[qtdDependentes];
+        double salarioFamilia=0;
+        for (int i = 0; i < qtdDependentes; i++) {
+            nomeDependentes[i] = JOptionPane.showInputDialog("Informe o nome do "+(i+1)+"° dependente");
+            salarioFamilia+=20;
+        }
+        System.out.println("Funcionário: "+ nome);
+        for (int i = 0; i < qtdDependentes; i++) {
+            System.out.println("Dependente "+i+": "+ nomeDependentes[i]);
+        }
+        System.out.println("Salário: R$"+(salario+salarioFamilia));
+    }
 }
--- src/atividade3/VetorReajuste.java
+++ src/atividade3/VetorReajuste.java
@@ -3,4 +3,5 @@ package atividade3;
 public class VetorReajuste {
     //Faça um programa que leia 10 salários.
     // Informe um percentual de reajuste e aplique aos 10 salários.
+
 }
